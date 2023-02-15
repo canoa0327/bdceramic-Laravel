e15c7e8ca03a6fe7ddd2a525395ad71da806a5c1
SeongKon <canoa950327@gmail.com>
2023-02-15 17:16:41 +0900

    First commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title></title>
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+  <link href="https://hangeul.pstatic.net/hangeul_static/css/nanum-square.css" rel="stylesheet">
+  <link rel="stylesheet" href="{{ URL::asset('assets/css/style.css') }}">
+  <link rel="stylesheet" href="{{ URL::asset('assets/css/mobile.css') }}">
+  <link rel="stylesheet" href="{{ URL::asset('assets/css/carousel.css') }}">
+  <link rel="stylesheet" href="{{ URL::asset('assets/css/owl.carousel.min.css') }}">
+  <link rel="stylesheet" href="{{ URL::asset('assets/css/owl.theme.default.min.css') }}">
+  <link rel="stylesheet" href="{{ URL::asset('assets/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ URL::asset('assets/css/products.css') }}">
+</head>
+
+<body>
+  <div class="flex-center position-ref full-height">
+    @if (Route::has('login'))
+    <div class="top-right links">
+      @auth
+      <a href="{{ url('/home') }}">Home</a>
+      @else
+      <a href="{{ route('login') }}">Login</a>
+      <a href="{{ route('register') }}">Register</a>
+      @endauth
+    </div>
+    @endif
+
+    <div class="content">
+      <div class="title m-b-md">
+        Laravel
+      </div>
+
+      <div class="links">
+        <a href="https://laravel.com/docs">Documentation</a>
+        <a href="https://laracasts.com">Laracasts</a>
+        <a href="https://laravel-news.com">News</a>
+        <a href="https://forge.laravel.com">Forge</a>
+        <a href="https://github.com/laravel/laravel">GitHub</a>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
